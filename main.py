04ec1740905bf72339e87d069de9e50add923ab8
Theo Fairchild-Coppoletti <theo@fcfamily.com>
2022-01-28 11:46:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import config
 import csv
 import requests
 import json
-import lxml.html
 
 from nscraper import NScraper
 from page_actions import ACTIONS
@@ -44,20 +43,6 @@ FIELDS = [
 ]
 
 
-def get_proxies():
-    url = 'https://free-proxy-list.net/'
-
-    response = requests.get(url)
-    parser = lxml.html.fromstring(response.text)
-    proxies = []
-    for i in parser.xpath('//tbody/tr')[:100]:
-        if i.xpath('.//td[7][contains(text(),"yes")]'):
-            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
-            proxies.append(proxy)
-
-    return proxies
-
-
 def write_header():
     with open("data_main.csv", 'a') as csvfile:
         write = csv.writer(csvfile)
@@ -90,7 +75,6 @@ def get_coords(row):
 
 
 def main():
-
     write_header()
 
     nScraper = NScraper(config.HEADERS, delay=5)
